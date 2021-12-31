7f1a17b24e9dd722127941562b84ab0ad45e87b2
Patrick Ashamalla <67871+patrickash@users.noreply.github.com>
2021-12-31 00:29:22 -0500

    update users

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.get('/', function(req, res){
 
 app.get('/users', function(req, res) {
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmailx.com"},
+    {"id": 2, "firstName":"Tammy", "lastName":"Norton", "email":"tnorton@yahoox.com"},
+    {"id": 3, "firstName":"Tina", "lastName":"Lee", "email":"lee.tina@hotmailx.com"}
   ]);
 });
 
